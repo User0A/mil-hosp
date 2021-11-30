91efda8d2560f622b2a9ff58f388db647c24d91a
Руслан <kovbaruslan@gmail.com>
2021-11-30 11:05:26 +0200

    Everthing is done

---
+++ models/Hospital.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Illness = require('./Illness.js');
+
+let Hospital = new Schema({
+  name: String,
+  region: String,
+  // illnesses: [{
+  //   type: Schema.Types.ObjectId,
+  //   ref: "Illness"
+  // }
+  // ]
+});
+
+module.exports = mongoose.model('Hospital', Hospital);
